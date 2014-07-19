67e78f2157715ce3be7a9ce87ecc893070d38c73
Jisan Zaman <jzthegreat@gmail.com>
2014-07-19 17:43:20 -0400

    added getting all districts example

--- examples.js
+++ examples.js
@@ -3,7 +3,12 @@ var NYTDistricts = require('./nytdistricts');
 var nytAPIkey = apiKeys.nytAPIkey
 var nytdistricts = new NYTDistricts(nytAPIkey);
 
+nytdistricts.getDistricts(40.7115760,-73.7972060, function(err,data){
+  if(err) throw err;
+  console.log("my districts", data)
+})
+
 nytdistricts.getCityCouncilDistrict(40.7115760,-73.7972060, function(err,data){
   if(err) throw err;
-  console.log("my districts: ",data)
+  console.log("my City Council district: ",data)
 })
